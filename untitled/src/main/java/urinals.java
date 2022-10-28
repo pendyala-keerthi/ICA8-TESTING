65d8f51f953c2ca003515e0152960bd9c06725e8
keerthi pendyala <kpendya1@asu.edu>
2022-10-27 22:50:19 -0700

    first commit

---
+++ untitled/src/main/java/urinals.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class urinals {
+    public static void main(String[] args) {
+
+    }
+    public static boolean goodString(String str){
+
+        System.out.println("Not Yet Executed");
+        return true;
+
+    }
+
+
+}
---
+++ untitled/src/test/java/urinalsTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class urinalsTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void goodString() {
+
+        System.out.println("====== Keerthi Pendyala == TEST ONE EXECUTED =======");
+        assertEquals(false,urinals.goodString("Helloworld!!!"));
+    }
+}
