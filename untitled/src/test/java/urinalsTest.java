b451b247a7d28dbfa7586f4d3a0f2717a70e9a4a
keerthi pendyala <kpendya1@asu.edu>
2022-10-27 23:27:55 -0700

    01000 test case commit

--- untitled/src/test/java/urinalsTest.java
+++ untitled/src/test/java/urinalsTest.java
@@ -12,7 +12,7 @@ class urinalsTest {
     void goodString() {
 
         System.out.println("Keerthi Pendyala");
-        assertEquals(true,urinals.goodString("0000"));
+        assertEquals(true,urinals.goodString("01000"));
     }
     @Test
     void countUrinals() {
